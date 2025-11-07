0090186cb9d633402819fc497bd868ef8267270a
hieund18 <hieucmndt6419+github@gmail.com>
2025-11-07 20:42:37 +0700

    update

---
+++ order-service-db.sql
@@ -0,0 +1,21 @@
+create database order_service;
+use order_service;
+
+create table assets(
+	id nvarchar(50) primary key,
+    user_id nvarchar(50) not null,
+    coin_id varchar(20) not null,
+    quantity DECIMAL(30, 5) not null,
+    buy_price DECIMAL(30, 2) not null
+);
+
+create table trade_histories(
+	id nvarchar(50) primary key,
+    user_id nvarchar(50) not null,
+    coin_id varchar(20) not null,
+    type nvarchar(10),
+    quantity DECIMAL(30, 5) not null,
+    price DECIMAL(30, 2) not null,
+    amount DECIMAL(30, 2) not null,
+    created_at datetime
+);
